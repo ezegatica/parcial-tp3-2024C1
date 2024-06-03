880cbc7a16133f9d30c4e263e27ab060687d3bfd
Facundo Costa <costafacu@gmail.com>
2024-06-03 19:28:36 -0300

    Ammend de profile

--- app/src/main/java/com/example/simulacros/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/simulacros/ui/profile/ProfileFragment.kt
@@ -32,12 +32,6 @@ class ProfileFragment : Fragment() {
 
     private val viewModel: ProfileViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
